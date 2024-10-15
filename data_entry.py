3b61aadfaaba7792caa5c0cf5e5a3e39a1a532f0
Vincent Bui <vincent.bui657@gmail.com>
2024-10-15 19:44:09 -0400

    Add function for getting description

--- data_entry.py
+++ data_entry.py
@@ -2,7 +2,8 @@
 from datetime import datetime
 
 date_format = "%d-%m-%Y"
-CATEGORIES = {"I": "Income", "E": "Expense" }
+CATEGORIES = {"I": "Income", "E": "Expense"}
+
 
 def get_date(prompt, allow_default=False):
     date_str = input(prompt)
@@ -13,26 +14,29 @@ def get_date(prompt, allow_default=False):
         valid_date = datetime.strptime(date_str, date_format)
         return valid_date.strftime(date_format)
     except ValueError:
-        print("Invalid date format. Pleas enter the date in dd-mm-yyyy format")
+        print("Invalid date frmat. Please enter the date in dd-mm-yyyy format")
         return get_date(prompt, allow_default)
-    
+
+
 def get_amount():
     try:
-        amount = float(input("Enter the amount:"))
+        amount = float(input("Enter the amount: "))
         if amount <= 0:
             raise ValueError("Amount must be a non-negative non-zero value.")
         return amount
     except ValueError as e:
         print(e)
         return get_amount()
-    
+
+
 def get_category():
     category = input("Enter the category ('I' for Income or 'E' for Expense): ").upper()
     if category in CATEGORIES:
         return CATEGORIES[category]
-    
+
     print("Invalid category. Please enter 'I' for Income or 'E' for Expense.")
     return get_category()
 
-def description():
-    pass
+
+def get_descriptipn():
+    return input("Enter a description (optional): ")
